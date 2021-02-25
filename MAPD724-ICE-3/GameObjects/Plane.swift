c2f7c59e3f77baac8a2726a6bd7f7bca50bf0238
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2021-02-24 22:34:29 -0500

    Add plane class

--- MAPD724-ICE-3/GameObjects/Plane.swift
+++ MAPD724-ICE-3/GameObjects/Plane.swift
@@ -1,10 +1,3 @@
-//
-//  Plane.swift
-//  MAPD724-ICE-3
-//
-//  Created by Raj Kumar Shahu on 2021-02-24.
-//
-
 import SpriteKit
 import GameplayKit
 
@@ -20,52 +13,35 @@ class Plane: GameObject {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // LifeCycle functions
+    // LifeCycle Functions
     
-    override func CheckBounds() {
+    override func CheckBounds()
+    {
         // constrain on the left - left boundary
         if(position.x <= -310){
             position.x = -310
         }
         
         // constrain on the right - right boundary
-        if(position.x >= -310){
-            position.x = -310
+        if(position.x >= 310){
+            position.x = 310
         }
-        
     }
     
-    
-    
     override func Reset() {
-        position.y = 730
-        
-        // get a pseudo-random number from -313 to 313 =
-        let randomX:Int = (randomSource?.nextInt(upperBound: 626))! - 313
-        
-        position.x = CGFloat(randomX)
-        isColliding = false
-        
-        
+       
     }
     
     // initialization
     override func Start() {
-        zPosition = 1
-        
-        Reset()
-        dy = 5.0
+        zPosition = 2
     }
     
     override func Update() {
-        Move()
         CheckBounds()
     }
     
-    func Move() {
-        position.y -= dy!
+    func TouchMove(newPos: CGPoint) {
+        position = newPos
     }
-    
-    
 }
-
